c66e2507b5f1923bec584164df0237b61a4935d5
ahmedalaqib <ahmedalaqib@gmail.com>
2023-08-22 15:30:30 +0000

    Write and reset the buffer

---
+++ 11-reset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+/**
+*reset - print and reset the local buffer of printf
+*@buffer : local buffer of printf
+*@j : index of buffer
+*Return: nothing
+*/
+void reset(char *buffer, int *j)
+{
+	int i;
+
+	write(1, buffer, 1024);
+	for (i = 0; i < 1024; i++)
+	{
+		buffer[i] = 0;
+	}
+	(*j) = 0;
+}
